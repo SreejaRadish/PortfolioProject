1b21eea84956e7c83ad49da6865938b8ceb3b3d0
Sreeja R <SreejaRadish@gmail.com>
2021-08-05 21:14:07 +0100

    Website is made responsive

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var button=document.getElementById('projectBtn');
+var projectButton=document.getElementById('projectBtn');
 function btnClickFunction(){
     location.assign('#projects');
+}
+var skillButton=document.getElementById('skillsBtn');
+function btnClickFunctionSkills(){
+    location.assign('#skills');
 }
